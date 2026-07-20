3ffb3a033afdd266b2c07aa99e6f2e5a7de67ba4
Lucas Silva <lucasdevcontato64@gmail.com>
2026-07-20 19:22:27 -0300

    Primeiro commit

---
+++ Module02 - Frontend/clarim-diario/src/data/noticias.js
@@ -0,0 +1,30 @@
+export const noticias = [
+    {
+        id: 1,
+        categoria: 'Ameaças Urbanas',
+        titulo: 'Homem-Aranha destrói viaduto e chama de "resgate"', 
+        resumo: 'Prejuízo estimado em milhões. Prefeitura estuda enviar a conta ao vigilante mascarado',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        id: 2,
+        categoria: 'Cidade',
+        titulo: 'Metrô da linha vermelha tem horário estendido no verão',
+        resumo: 'Medida atende antigo pedido dos moradores do Queens',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        id: 3,
+        categoria: 'Opinião do Editor',
+        titulo: 'Máscaras escondem rostos e intenções',
+        resumo: 'Se ele fosse herói de verdade, mostraria a cara. Simples assim.',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        id: 4,
+        categoria: 'Esportes',
+        titulo: 'Eddie Brock Lesionado', 
+        resumo: 'Eddie Brock, A.K.A Venon (Apelido do atleta) está se tratando com Dr. Connors',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }
+]
